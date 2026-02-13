e1108fe520555457b0876b7dfb5623abe03783bd
yufu333 <yufu3@yufusch.onmicrosoft.com>
2026-02-13 15:32:04 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from pyodide.ffi import create_proxy
 deck = list(range(52))
 
 def draw_two_cards(*args, **kwargs):
-  # デッキをシャッフルして、先頭2枚を取り出す
-  random.shuffle(deck)
-  c1, c2 = deck[0], deck[1]
+    # デッキをシャッフルして、先頭2枚を取り出す
+    random.shuffle(deck)
+    c1, c2 = deck[0], deck[1]
 
-  # JavaScript の show_card(cardId, canvasId) を呼び出す
-  js.show_card(int(c1), "cardCanvas1")
-  js.show_card(int(c2), "cardCanvas2")
+    # JavaScript の show_card(cardId, canvasId) を呼び出す
+    js.show_card(int(c1), "cardCanvas1")
+    js.show_card(int(c2), "cardCanvas2")
 
-# JS から呼べるように Python 関数を登録
-# window.onDrawClicked = draw_two_cards に相当
-js.window.onDrawClicked = pyodide.create_proxy(draw_two_cards)
+    # JS から呼べるように Python 関数を登録
+    # window.onDrawClicked = draw_two_cards に相当
+    js.window.onDrawClicked = create_proxy(draw_two_cards)
 
 # ページ読み込み直後にも1回引いておく（お好みで）
 draw_two_cards()
